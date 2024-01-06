f2b8278dce890037a3770ee632427606247c85de
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-06 16:27:26 -0600

    Add Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import NewPost from "./components/NewPost";
+import { useState } from "react";
+import MainHeader from "./components/MainHeader";
 import PostsList from "./components/PostsList";
 
 function App() {
+  const [modalIsVisable, setModalIsVisable] = useState(false);
+
+  function hideModalHandler() {
+    setModalIsVisable(false);
+  }
+
+  function showModalHandler() {
+    setModalIsVisable(true);
+  }
+
   return (
     <>
-      <NewPost />
+      <MainHeader onCreatePost={showModalHandler} />
       <main>
-        <PostsList />
+        <PostsList
+          isPosting={modalIsVisable}
+          onStopPosting={hideModalHandler}
+        />
       </main>
     </>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
 
-function PostsList() {
+function PostsList({ isPosting, onStopPosting }) {
+  const [enteredText, setEnterText] = useState("");
+  const [enteredAuthor, setEnterAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setEnterText(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnterAuthor(event.target.value);
+  }
+
   return (
-    <ul className={styles.posts}>
-      <Post author="Kimmy" text="Kimmy has 3 color!" />
-      <Post author="Mini" text="Mini is the calmest!" />
-      <Post author="Hockeye" text="Mini is the youngest!" />
-    </ul>
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={changeBodyHandler}
+            onAuthorChange={changeAuthorHandler}
+            onCancel={onStopPosting}
+          />
+        </Modal>
+      )}
+
+      <ul className={styles.posts}>
+        <Post author={enteredAuthor} text={enteredText} />
+        <Post author="Mini" text="Mini is the calmest!" />
+        <Post author="Hockeye" text="Mini is the youngest!" />
+      </ul>
+    </>
   );
 }
 
