eba3caee5adb4734b0df564567e22c860eff66c2
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-13 00:54:47 -0600

    Connect POST Backend

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,12 +2,29 @@ import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    // send request to backend app
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     // update state based on previous state
     setPosts((existingPost) => [postData, ...existingPost]);
   }
