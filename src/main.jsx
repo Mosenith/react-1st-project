daf3186ca3dfbf218ea1b44b9a9d6c7d8a819ea3
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-01-15 19:17:38 -0600

    Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import NewPost from "./components/NewPost.jsx";
+import MainHeader from "./components/MainHeader.jsx";
+import RootLayout from "./routes/RootLayout.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      {
+        path: "/create-posts",
+        element: <NewPost />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
